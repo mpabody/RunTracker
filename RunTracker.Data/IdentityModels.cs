9c4b24566ae2013df0011560e0f98c47f892c805
Michael Pabody <michael.pabody@gmail.com>
2019-09-12 20:17:20 -0400

    added shoe class in data

--- RunTracker.Data/IdentityModels.cs
+++ RunTracker.Data/IdentityModels.cs
@@ -4,6 +4,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using RunTracker.Data;
 
 namespace BlueBadgeRunTracker.Data
 {
---
+++ RunTracker.Data/Shoe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTracker.Data
+{
+    public class Shoe
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public Guid UserID { get; set; }
+
+        // maybe add nickname or type (ex. "racing flat")
+    }
+}
